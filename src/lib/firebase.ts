38e3d1615be040d39558e9295af63625b9719b67
SPECT3R0 <junaid01arshad@gmail.com>
2025-05-23 20:16:46 +0500

    Add all project files

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvUStwQu2lPrsSAaTMb9x3oVNzp0KyGRA",
+  authDomain: "spect3r-spm.firebaseapp.com",
+  projectId: "spect3r-spm",
+  storageBucket: "spect3r-spm.firebasestorage.app",
+  messagingSenderId: "647567100345",
+  appId: "1:647567100345:web:4b3e361a6a6273efe74e68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
